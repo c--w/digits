ee0e6e0ccaed695d5d00189ac1fea87842774204
c--w <zevrnc@gmail.com>
2023-04-21 09:38:12 +0200

    better numbers, winner anim

--- main.js
+++ main.js
@@ -4,26 +4,25 @@ window.onclick = (event) => handleClick(event);
 var MAX = 25;
 var numbers;
 var toguess = 0;
-var oper = ['+', '-', '*', '/'];
-var seed;
+var oper = ['*', '+', '-', '/'];
 var undo_stack = [];
 var current_numbers;
 var total_time = 0;
 var games = 0;
 var start_time = 0;
-seed = Number(getSeed() + '0000');
+var seed;
+initSeed();
 function initGame() {
-    start_time = Date.now();
+    $('#undo').addClass('empty');
     numbers = [];
     for (let i = 0; i < 6; i++) {
-        let n = Math.ceil(rand(seed) * rand(seed) * MAX);
+        let n = Math.floor(Math.pow(rand(), 1.5) * MAX) + 1;
         numbers.push(n);
-        seed++;
     }
     let numbersSorted = [...numbers]
     numbersSorted.sort((a, b) => a - b);
     console.log(numbers);
-    num_oper = Math.round(rand(seed) * 1) + 4;
+    num_oper = Math.round(rand() + 4);
     let obj;
     do {
         obj = generateGuess(num_oper, [...numbers]);
@@ -39,6 +38,7 @@ function initGame() {
     $("#time").text(total_time);
     if (games)
         $("#avg").text(Math.round(total_time / games));
+    start_time = Date.now();
 }
 
 function fillNumbers(numbers) {
@@ -58,16 +58,16 @@ function generateGuess(num_oper, numbers) {
     let guess;
     let operations = [];
     for (let j = 0; j < num_oper; j++) {
-        seed++;
-        let first = numbers.splice(Math.floor(rand(seed) * rand(seed) * numbers.length), 1)[0];
-        seed++;
-        let second = numbers.splice(Math.floor(rand(seed) * numbers.length), 1)[0];
-        seed++;
-        let op = oper[Math.floor(rand(seed) * 4)];
-        let ev = first + op + second;
-        ev = ev.replace("--", "+");
-        ev = ev.replace("+-", "-");
-        guess = eval(ev);
+        let first = numbers.splice(Math.floor(Math.pow(rand(), 1.5) * numbers.length), 1)[0];
+        let second = numbers.splice(Math.floor(rand() * numbers.length), 1)[0];
+        let ev;
+        do {
+            let op = oper[Math.floor(rand() * 4)];
+            ev = first + op + second;
+            ev = ev.replace("--", "+");
+            ev = ev.replace("+-", "-");
+            guess = eval(ev);
+        } while(guess == 0 || guess == first || guess == second)
         numbers.unshift(guess);
         operations.push(ev);
     }
@@ -84,10 +84,13 @@ function handleClick(event) {
         if (undo_stack.length > 0) {
             current_numbers = undo_stack.pop();
             fillNumbers(current_numbers);
+            if(!undo_stack.length)
+                $('#undo').addClass('empty');
         }
     } else if (el.hasClass('number')) {
         if (first_selected() && oper_selected()) {
             undo_stack.push([...current_numbers]);
+            $('#undo').removeClass('empty');
             let first_index = $('.number.selected').data('i');
             let second_index = el.data('i');
             if (first_index == second_index) {
@@ -99,22 +102,28 @@ function handleClick(event) {
             var result = eval(first + oper + second);
             current_numbers[first_index] = 0;
             current_numbers[second_index] = result;
-            fillNumbers(current_numbers);
             $('.oper').removeClass('selected');
-            $('.number').removeClass('selected');
+            $($('.number')[first_index]).addClass('hidden');
+            $($('.number')[first_index]).removeClass('selected');
             $($('.number')[second_index]).addClass('selected');
+            $($('.number')[second_index]).text(result);
             if (result == toguess) {
+                $($('.number')[second_index]).addClass('winner');
                 games++;
                 total_time += Math.round((Date.now() - start_time) / 1000);
-                initGame();
+                setTimeout(initGame, 2000);
             }
         } else {
             if (el.hasClass('selected')) {
                 el.removeClass('selected');
                 $('.oper').removeClass('selected');
                 return;
+            } else {
+                let first_index = $('.number.selected').data('i');
+                if(first_index) {
+                    $($('.number')[first_index]).removeClass('selected');
+                }
             }
-            $('.number').removeClass('selected');
             el.addClass('selected');
         }
     } else if (el.hasClass('oper')) {
@@ -129,16 +138,18 @@ function handleClick(event) {
     }
 }
 
-function rand(a) {
-    let t = a += 0x6D2B79F5;
+function rand() {
+    seed++;    
+    let t = seed += 0x6D2B79F5;
     t = Math.imul(t ^ t >>> 15, t | 1);
     t ^= t + Math.imul(t ^ t >>> 7, t | 61);
     return ((t ^ t >>> 14) >>> 0) / 4294967296;
 }
 
-function getSeed() {
+function initSeed() {
     let now = new Date();
-    return now.toISOString().replaceAll("-", "").replaceAll("T", "").replaceAll(":", "").substring(2, 12);
+    seed = now.toISOString().replaceAll("-", "").replaceAll("T", "").replaceAll(":", "").substring(2, 12);
+    seed = Number(seed + '0000');
 }
 
 function tooEasy(guess, numList) {
